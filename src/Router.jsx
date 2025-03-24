562421697c608ca77058402ce20d1f6b92766baf
Henry Alfredo Garcia Palacio <henrygarcia-15@hotmail.com>
2025-03-23 22:29:08 -0500

    se reemplazan archivos problema

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
 import {
   Dashboard,
@@ -14,39 +14,29 @@ import {
   Geography,
   Calendar,
   Stream,
-  Login,
 } from "./scenes";
 
 const AppRouter = () => {
-  const location = useLocation();
-  const isLoginPage = location.pathname === "/login";
-
   return (
-    <div>
-      {!isLoginPage && <App />} {/* Oculta el App (que incluye el sidebar) en la página de login */}
+    <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/invoices" element={<Invoices />} />
-        <Route path="/form" element={<Form />} />
-        <Route path="/calendar" element={<Calendar />} />
-        <Route path="/bar" element={<Bar />} />
-        <Route path="/pie" element={<Pie />} />
-        <Route path="/stream" element={<Stream />} />
-        <Route path="/line" element={<Line />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/geography" element={<Geography />} />
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/invoices" element={<Invoices />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/bar" element={<Bar />} />
+          <Route path="/pie" element={<Pie />} />
+          <Route path="/stream" element={<Stream />} />
+          <Route path="/line" element={<Line />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/geography" element={<Geography />} />
+        </Route>
       </Routes>
-    </div>
+    </Router>
   );
 };
 
-export default function AppWrapper() {
-  return (
-    <Router>
-      <AppRouter />
-    </Router>
-  );
-}
+export default AppRouter;
